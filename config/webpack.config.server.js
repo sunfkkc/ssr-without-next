da0862cbabeabbf3956efb7d553592882ac4295f
sunfkkc <rlcks77@nate.com>
2023-08-09 13:07:14 +0900

    문법 수정

--- config/webpack.config.server.js
+++ config/webpack.config.server.js
@@ -11,148 +11,146 @@ const sassModuleRegex = /\.module\.(scss|sass)$/;
 
 const env = getClientEnvironment(paths.publicUrlOrPath.slice(0, -1));
 
-module.exports = function () {
-  return {
-    mode: 'production',
-    entry: 'paths.ssrIndexJs',
-    target: 'node',
-    output: {
-      path: paths.ssrBuild,
-      filename: 'server.js',
-      chunkFilename: 'js/[name].chunk.js',
-      publicPath: paths.publicUrlOrPath,
-    },
-    module: {
-      rules: [
-        {
-          oneOf: [
-            {
-              test: /\.(js|mjs|jsx|ts|tsx)$/,
-              include: paths.appSrc,
-              loader: require.resolve('babel-loader'),
-              options: {
-                customize: require.resolve(
-                  'babel-preset-react-app/webpack-overrides'
-                ),
-                presets: [
-                  [
-                    require.resolve('babel-preset-react-app'),
-                    {
-                      runtime: 'automatic',
-                    },
-                  ],
+module.exports = {
+  mode: 'production',
+  entry: paths.ssrIndexJs,
+  target: 'node',
+  output: {
+    path: paths.ssrBuild,
+    filename: 'server.js',
+    chunkFilename: 'js/[name].chunk.js',
+    publicPath: paths.publicUrlOrPath,
+  },
+  module: {
+    rules: [
+      {
+        oneOf: [
+          {
+            test: /\.(js|mjs|jsx|ts|tsx)$/,
+            include: paths.appSrc,
+            loader: require.resolve('babel-loader'),
+            options: {
+              customize: require.resolve(
+                'babel-preset-react-app/webpack-overrides'
+              ),
+              presets: [
+                [
+                  require.resolve('babel-preset-react-app'),
+                  {
+                    runtime: 'automatic',
+                  },
                 ],
+              ],
 
-                plugins: [
-                  [
-                    require.resolve('babel-plugin-named-asset-import'),
-                    {
-                      loaderMap: {
-                        svg: {
-                          ReactComponent:
-                            '@svgr/webpack?-svgo,+titleProp,+ref![path]',
-                        },
+              plugins: [
+                [
+                  require.resolve('babel-plugin-named-asset-import'),
+                  {
+                    loaderMap: {
+                      svg: {
+                        ReactComponent:
+                          '@svgr/webpack?-svgo,+titleProp,+ref![path]',
                       },
                     },
-                  ],
+                  },
                 ],
-                // This is a feature of `babel-loader` for webpack (not Babel itself).
-                // It enables caching results in ./node_modules/.cache/babel-loader/
-                // directory for faster rebuilds.
-                cacheDirectory: true,
-                // See #6846 for context on why cacheCompression is disabled
-                cacheCompression: false,
-                compact: false,
+              ],
+              // This is a feature of `babel-loader` for webpack (not Babel itself).
+              // It enables caching results in ./node_modules/.cache/babel-loader/
+              // directory for faster rebuilds.
+              cacheDirectory: true,
+              // See #6846 for context on why cacheCompression is disabled
+              cacheCompression: false,
+              compact: false,
+            },
+          },
+          {
+            test: cssRegex,
+            exclude: cssModuleRegex,
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1,
+              modules: {
+                exportOnlyLocals: true,
               },
             },
-            {
-              test: cssRegex,
-              exclude: cssModuleRegex,
-              loader: require.resolve('css-loader'),
-              options: {
-                importLoaders: 1,
-                modules: {
-                  exportOnlyLocals: true,
-                },
+          },
+          {
+            test: cssModuleRegex,
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1,
+              modules: {
+                exportOnlyLocals: true,
+                getLocalIdent: getCSSModuleLocalIdent,
               },
             },
-            {
-              test: cssModuleRegex,
-              loader: require.resolve('css-loader'),
-              options: {
-                importLoaders: 1,
-                modules: {
-                  exportOnlyLocals: true,
-                  getLocalIdent: getCSSModuleLocalIdent,
-                },
-              },
-            },
-            {
-              test: sassRegex,
-              exclude: sassModuleRegex,
-              use: [
-                {
-                  loader: require.resolve('css-loader'),
-                  options: {
-                    importLoaders: 3,
-                    modules: {
-                      exportOnlyLocals: true,
-                    },
+          },
+          {
+            test: sassRegex,
+            exclude: sassModuleRegex,
+            use: [
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  importLoaders: 3,
+                  modules: {
+                    exportOnlyLocals: true,
                   },
                 },
-                require.resolve('sass-loader'),
-              ],
-            },
-            {
-              test: sassRegex,
-              exclude: sassModuleRegex,
-              use: [
-                {
-                  loader: require.resolve('css-loader'),
-                  options: {
-                    importLoaders: 3,
-                    modules: {
-                      exportOnlyLocals: true,
-                      getLocalIdent: getCSSModuleLocalIdent,
-                    },
+              },
+              require.resolve('sass-loader'),
+            ],
+          },
+          {
+            test: sassRegex,
+            exclude: sassModuleRegex,
+            use: [
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  importLoaders: 3,
+                  modules: {
+                    exportOnlyLocals: true,
+                    getLocalIdent: getCSSModuleLocalIdent,
                   },
                 },
-                require.resolve('sass-loader'),
-              ],
-            },
-            {
-              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-              loader: require.resolve('resolve-url-loader'),
-              options: {
-                emitFile: false,
-                limit: 10000,
-                name: 'static/media/[name].[hash:8].[ext]',
               },
+              require.resolve('sass-loader'),
+            ],
+          },
+          {
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            loader: require.resolve('resolve-url-loader'),
+            options: {
+              emitFile: false,
+              limit: 10000,
+              name: 'static/media/[name].[hash:8].[ext]',
             },
-            {
-              // Exclude `js` files to keep "css" loader working as it injects
-              // its runtime that would otherwise be processed through "file" loader.
-              // Also exclude `html` and `json` extensions so they get processed
-              // by webpacks internal loaders.
-              loader: require.resolve('file-loader'),
-              exclude: [/^$/, /\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
-              options: {
-                emitFile: false,
-                name: 'static/media/[name].[hash:8].[ext]',
-              },
+          },
+          {
+            // Exclude `js` files to keep "css" loader working as it injects
+            // its runtime that would otherwise be processed through "file" loader.
+            // Also exclude `html` and `json` extensions so they get processed
+            // by webpacks internal loaders.
+            loader: require.resolve('file-loader'),
+            exclude: [/^$/, /\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+            options: {
+              emitFile: false,
+              name: 'static/media/[name].[hash:8].[ext]',
             },
-          ],
-        },
-      ],
-    },
-    resolve: {
-      modules: ['node_modules'],
-    },
-    externals: [
-      nodeExternals({
-        allowlist: [/@babel/],
-      }),
+          },
+        ],
+      },
     ],
-    plugins: [new webpack.DefinePlugin(env.stringified)],
-  };
+  },
+  resolve: {
+    modules: ['node_modules'],
+  },
+  externals: [
+    nodeExternals({
+      allowlist: [/@babel/],
+    }),
+  ],
+  plugins: [new webpack.DefinePlugin(env.stringified)],
 };
