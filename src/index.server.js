6afb61378c85a872aeb5b3b482e9adf9a15a8b70
sunfkkc <rlcks77@nate.com>
2023-08-11 11:53:33 +0900

    express서버를 만들고 html 렌더링

--- src/index.server.js
+++ src/index.server.js
@@ -1,7 +1,50 @@
 import ReactDomServer from 'react-dom/server';
+import express from 'express';
+import { StaticRouter } from 'react-router-dom/server';
+import App from './App';
+import path from 'path';
+import fs from 'fs';
 
-const html = ReactDomServer.renderToString(
-  <div>서버 사이드 렌더링 된 html 입니다.</div>
+const manifest = JSON.parse(
+  fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf-8')
 );
 
-console.log(html);
+function createPage(root) {
+  return `
+  <!doctype html>
+  <html lang="en">
+  <head><meta charset="utf-8"/><link rel="icon" href="/favicon.ico"/>
+  <meta name="viewport" content="width=device-width,initial-scale=1"/>
+  <meta name="theme-color" content="#000000"/><meta name="description" content="Web site created using create-react-app"/>
+  <link rel="apple-touch-icon" href="/logo192.png"/><link rel="manifest" href="/manifest.json"/>
+  <title>React App</title>
+  <link href="${manifest.files['main.css']}" rel="stylesheet"/>
+  </script>
+  </head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id="root">${root}</div>
+  <script src="${manifest.files['main.js']}"></script>
+  </body>
+  </html>
+  `;
+}
+const app = express();
+
+const serverRender = (req, res, next) => {
+  const context = {};
+  const jsx = (
+    <StaticRouter location={req.url}>
+      <App />
+    </StaticRouter>
+  );
+
+  const root = ReactDomServer.renderToString(jsx);
+  res.send(createPage(root));
+};
+
+const serve = express.static(path.resolve('./build'), { index: false });
+
+app.use(serve);
+app.use(serverRender);
+
+app.listen(5000, () => {
+  console.log('5000번 포트에서 실행중');
+});
