670e158d117aa9e4efd8049bffc3ae201078b09b
sunfkkc <rlcks77@nate.com>
2023-08-12 00:02:14 +0900

    서버에서 queryClient 주입

--- src/index.server.js
+++ src/index.server.js
@@ -4,12 +4,19 @@ import { StaticRouter } from 'react-router-dom/server';
 import App from './App';
 import path from 'path';
 import fs from 'fs';
+import {
+  dehydrate,
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query';
+import axios from 'axios';
 
 const manifest = JSON.parse(
   fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf-8')
 );
 
-function createPage(root) {
+function createPage(root, dehydratedState) {
   return `
   <!doctype html>
   <html lang="en">
@@ -19,25 +26,41 @@ function createPage(root) {
   <link rel="apple-touch-icon" href="/logo192.png"/><link rel="manifest" href="/manifest.json"/>
   <title>React App</title>
   <link href="${manifest.files['main.css']}" rel="stylesheet"/>
-  </script>
   </head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id="root">${root}</div>
   <script src="${manifest.files['main.js']}"></script>
+  <script>
+          window.__REACT_QUERY_STATE__ = ${JSON.stringify(dehydratedState)};
+        </script>
   </body>
   </html>
   `;
 }
 const app = express();
 
-const serverRender = (req, res, next) => {
-  const context = {};
+const serverRender = async (req, res, next) => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery('users', async () => {
+    const { data } = await axios.get(
+      'https://jsonplaceholder.typicode.com/users'
+    );
+    return data;
+  });
+  const dehydratedState = dehydrate(queryClient);
+
   const jsx = (
-    <StaticRouter location={req.url}>
-      <App />
-    </StaticRouter>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={dehydratedState}>
+        <StaticRouter location={req.url}>
+          <App />
+        </StaticRouter>
+      </Hydrate>
+    </QueryClientProvider>
   );
 
   const root = ReactDomServer.renderToString(jsx);
-  res.send(createPage(root));
+  res.send(createPage(root, dehydratedState));
+
+  queryClient.clear();
 };
 
 const serve = express.static(path.resolve('./build'), { index: false });
