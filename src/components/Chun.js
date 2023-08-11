102a9d7d7aed7de70b808e405e86385b3fa6fcc0
sunfkkc <rlcks77@nate.com>
2023-08-12 00:04:43 +0900

    chunsik 페이지 초기 렌더링 시 users 데이터 표시 가능

--- src/components/Chun.js
+++ src/components/Chun.js
@@ -1,7 +1,27 @@
+import axios from 'axios';
 import React from 'react';
+import { useQuery } from 'react-query';
 
 function Chun() {
-  return <img alt="chunsik" src="/chun.png" width={200} height={200} />;
+  const { data } = useQuery(
+    'users',
+    async () => {
+      const { data } = await axios.get(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      return data;
+    },
+    { keepPreviousData: true }
+  );
+
+  return (
+    <div>
+      <img alt="chunsik" src="/chun.png" width={500} height={500} />
+      {data?.map((v) => (
+        <div>{v.name}</div>
+      ))}
+    </div>
+  );
 }
 
 export default Chun;
